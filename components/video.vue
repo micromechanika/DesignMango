8c4b62ec0e0488fcb1e0f5777836922614682f7d
micromechanika <micromechanika@gmail.com>
2020-04-28 04:18:04 +0300

    add video in site

---
+++ components/video.vue
@@ -0,0 +1,19 @@
+<template>
+	<video controls autoplay muted loop poster="assets/video/roadster.jpg">
+		<source src="@/assets/video/Roadster.mp4" type="video/mp4" />
+		Элемент video не поддерживается вашим браузером.
+	</video>
+</template>
+
+<script>
+	export default {
+		name: 'Video',
+	}
+</script>
+
+<style scoped lang="scss">
+	video {
+		width: 100%;
+		height: 800px;
+	}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,30 @@
 <template>
-	<div class="container">
-		<Header />
+	<div class="mediaSection">
+		<div class="container">
+			<Header />
+		</div>
+		<div class="videoSection">
+			<Video />
+		</div>
 	</div>
 </template>
 
 <script>
 	import Header from '../components/header'
-
+	import Video from '../components/video'
 	export default {
-		components: { Header },
+		components: { Video, Header },
 	}
 </script>
 
 <style lang="scss">
 	.container {
 		@include container;
+	}
+	.videoSection {
+		position: absolute;
+		left: 0;
+		top: 0;
+		z-index: -100;
 	}
 </style>
