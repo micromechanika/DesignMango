aa679d0df4505f5c10d23276b968097e5ae8588a
micromechanika <micromechanika@gmail.com>
2020-04-28 07:00:52 +0300

    gradient

--- components/video.vue
+++ components/video.vue
@@ -1,10 +1,11 @@
 <template>
 	<div class="myVideo">
-		<img v-show="!show" src="../assets/video/logoVideo.jpg" @click="show = !show" />
-		<video v-show="show" autoplay muted loop @dblclick="show = !show">
+		<img v-show="!show" src="../assets/video/logoVideo.png" />
+		<video v-show="show" autoplay muted loop>
 			<source src="../assets/video/Roadster.mp4" type="video/mp4" />
 			Элемент video не поддерживается вашим браузером.
 		</video>
+		<div class="gradient" @click="show = !show"></div>
 	</div>
 </template>
 
@@ -21,12 +22,19 @@
 
 <style scoped lang="scss">
 	.myVideo {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
 		width: 100%;
 		height: 800px;
+		.gradient {
+			position: absolute;
+			left: 0;
+			top: 0;
+			bottom: 0;
+			right: 0;
+			display: block;
+			width: 100%;
+			height: 100%;
+			background: linear-gradient(180deg, rgba(5, 4, 30, 0.15) 0%, #05041e 100%);
+		}
 		img {
 			width: 100%;
 			height: 100%;
