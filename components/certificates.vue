d8237c3d7af80d5e7a14fca8fe047b21ce087d3b
micromechanika <micromechanika@gmail.com>
2020-04-28 12:01:23 +0300

    require assets certificates

--- components/certificates.vue
+++ components/certificates.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="techonologiespanel">
-		<img v-for="(img, index) in techologiesImages" :key="index" :src="`${img.src}`" :alt="`${img.alt}`" />
+		<img v-for="(img, index) in techologiesImages" :key="index" :src="require(`@/assets/img/certificates/${img.src}`)" :alt="`${img.alt}`" />
 	</div>
 </template>
 
@@ -13,32 +13,20 @@
 				default() {
 					return [
 						{
-							src: 'https://ik.imagekit.io/nz3xsf2fh/proof/FB-mango_rkgftF2JPv.png',
-							alt: 'FB-mango',
+							src: 'vue.svg',
+							alt: 'vue',
 						},
 						{
-							src: 'https://ik.imagekit.io/nz3xsf2fh/proof/kfwmango_OwTU3Q9nIM.png',
-							alt: 'kfwmango',
+							src: 'tuv.svg',
+							alt: 'tuv',
 						},
 						{
-							src: 'https://ik.imagekit.io/nz3xsf2fh/proof/VueLogoMango_6EWjffxdT.png',
-							alt: 'VueLogoMango',
+							src: 'nuxt.svg',
+							alt: 'nuxt',
 						},
 						{
-							src: 'https://ik.imagekit.io/nz3xsf2fh/proof/Nuxtlogo-mangowebdesign_s-j8eFNr9.png',
-							alt: 'Nuxtlog',
-						},
-						{
-							src: 'https://ik.imagekit.io/nz3xsf2fh/proof/mangogernany_f0mS08t6CX.png',
-							alt: 'mangogernany',
-						},
-						{
-							src: 'https://ik.imagekit.io/nz3xsf2fh/proof/mangoexpert_rax2gfmRa.png',
-							alt: 'mangoexper',
-						},
-						{
-							src: 'https://ik.imagekit.io/nz3xsf2fh/proof/mangotuv-icon_CEYk3gXyL.png',
-							alt: 'mangotuv-icon',
+							src: 'kfw.svg',
+							alt: 'kfw',
 						},
 					]
 				},
@@ -49,12 +37,14 @@
 
 <style lang="scss" scoped>
 	.techonologiespanel {
+		margin: 2.68em 0;
 		display: flex;
 		flex-direction: row;
+		align-items: center;
 		justify-content: space-around;
 		img {
 			width: auto;
-			height: 2.68em;
+			height: 5.5em;
 		}
 	}
 </style>
