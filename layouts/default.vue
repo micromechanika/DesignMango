eef9d36787ddc0e6f70be9e1004bcecc53b5583d
micromechanika <micromechanika@gmail.com>
2020-05-03 07:08:05 +0300

    fix scss bug

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,4 +4,20 @@
 	</div>
 </template>
 
-<style></style>
+<style lang="scss">
+	* {
+		margin: 0;
+		padding: 0;
+		@include baseProps;
+		@include font;
+		font-family: 'Poppins', sans-serif;
+	}
+
+	html {
+		font-size: 0.8vw;
+	}
+
+	body {
+		background-color: $bg;
+	}
+</style>
