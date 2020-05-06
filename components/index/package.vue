17c1ed359bc67939b7df920329dfbefe8484f039
micromechanika <micromechanika@gmail.com>
2020-05-06 20:01:08 +0300

    build card

--- components/index/package.vue
+++ components/index/package.vue
@@ -21,40 +21,14 @@
 						<h3>Visitor statistics</h3>
 					</div>
 				</div>
-			</article>
-			<article>
-				<div class="info">
-					<h2>Starter</h2>
-					<div class="inPackage">
-						<img src="@/assets/img/like.svg" alt="like" />
-						<h3>Own domain to connect</h3>
-					</div>
-					<div class="inPackage">
-						<img src="@/assets/img/like.svg" alt="like" />
-						<h3>Own domain to connect</h3>
-					</div>
-					<div class="inPackage">
-						<img src="@/assets/img/like.svg" alt="like" />
-						<h3>Visitor statistics</h3>
-					</div>
+				<div class="price">
+					<p class="curensy">$</p>
+					<p class="rate">39,99</p>
+					<p class="time"><span>/</span>mo</p>
 				</div>
-			</article>
-			<article>
-				<div class="info">
-					<h2>Starter</h2>
-					<div class="inPackage">
-						<img src="@/assets/img/like.svg" alt="like" />
-						<h3>Own domain to connect</h3>
-					</div>
-					<div class="inPackage">
-						<img src="@/assets/img/like.svg" alt="like" />
-						<h3>Own domain to connect</h3>
-					</div>
-					<div class="inPackage">
-						<img src="@/assets/img/like.svg" alt="like" />
-						<h3>Visitor statistics</h3>
-					</div>
-				</div>
+				<button type="submit">
+					<p>Get started</p>
+				</button>
 			</article>
 		</section>
 	</div>
@@ -70,7 +44,6 @@
 	.package {
 		margin: 0 auto;
 		width: 67%;
-		height: 80vh;
 	}
 	.message {
 		width: 100%;
@@ -92,10 +65,9 @@
 			color: $green;
 		}
 	}
-
 	section {
 		width: 100%;
-		height: 100%;
+		height: 80vh;
 		margin: 2.5rem 0;
 		display: flex;
 		flex-direction: row;
@@ -104,8 +76,45 @@
 	article {
 		color: $white;
 		width: 33.3333%;
-		height: 100%;
 		border: 0.125rem solid $white;
+		.price {
+			width: 100%;
+			height: 14vh;
+			border: 0.125rem solid $white;
+			display: flex;
+			flex-direction: row;
+			justify-content: center;
+			align-items: center;
+			color: $white;
+			.curensy {
+				margin: 0 0.8rem 0 0;
+			}
+			.curensy,
+			.rate {
+				font-weight: 500;
+				font-size: 3rem;
+				line-height: 4.5rem;
+			}
+			.time {
+				font-weight: 500;
+				font-size: 1.5rem;
+				line-height: 2.25rem;
+				span {
+					margin: 0 0.8rem;
+				}
+			}
+		}
+		button {
+			width: 100%;
+			height: 14vh;
+			border: 0.125rem solid $bg;
+			background-color: $white;
+			p {
+				color: $bg;
+				font-size: 1.5rem;
+				line-height: 130%;
+			}
+		}
 	}
 	.inPackage {
 		margin: 2rem 0;
@@ -126,11 +135,12 @@
 	}
 	.info {
 		width: 72%;
+		height: 56vh;
 		margin: 0 auto;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		justify-content: center;
+		justify-content: flex-start;
 		h2 {
 			margin: 1.75rem 0 3.125rem 0;
 			font-size: 2rem;
