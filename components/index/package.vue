a25111cdea17061e688bd01a798d5870683db20c
micromechanika <micromechanika@gmail.com>
2020-05-07 10:33:33 +0300

    add props in buton

--- components/index/package.vue
+++ components/index/package.vue
@@ -5,26 +5,18 @@
 			<h3>perfect package</h3>
 		</div>
 		<section>
-			<article>
+			<article v-for="(item, pakageKey) in packages" :key="pakageKey">
 				<div class="info">
-					<h2>Starter</h2>
-					<div class="inPackage">
+					<h2>{{ item.name }}</h2>
+					<div class="inPackage" v-for="(include, includeKey) in item.includes" :key="includeKey">
 						<img src="@/assets/img/like.svg" alt="like" />
-						<h3>Own domain to connect</h3>
-					</div>
-					<div class="inPackage">
-						<img src="@/assets/img/like.svg" alt="like" />
-						<h3>Own domain to connect</h3>
-					</div>
-					<div class="inPackage">
-						<img src="@/assets/img/like.svg" alt="like" />
-						<h3>Visitor statistics</h3>
+						<h3>{{ include.offers }}</h3>
 					</div>
 				</div>
 				<div class="price">
-					<p class="curensy">$</p>
-					<p class="rate">39,99</p>
-					<p class="time"><span>/</span>mo</p>
+					<p class="curensy">{{ item.curensy }}</p>
+					<p class="rate">{{ item.rate }}</p>
+					<p class="time"><span>/</span>{{ item.time }}</p>
 				</div>
 				<button type="submit">
 					<p>Get started</p>
@@ -37,6 +29,36 @@
 <script>
 	export default {
 		name: 'Package',
+		props: {
+			packages: {
+				type: Array,
+				default() {
+					return [
+						{
+							name: 'Pro',
+							includes: [{ offers: 'Own domain to connect' }, { offers: 'Own domain to connect' }, { offers: 'Visitor statistics' }],
+							curensy: '$',
+							rate: '69,99',
+							time: 'mo',
+						},
+						{
+							name: 'Business',
+							includes: [{ offers: 'Own domain to connect' }, { offers: 'Own domain to connect' }, { offers: 'Visitor statistics' }],
+							curensy: '$',
+							rate: '39,99',
+							time: 'mo',
+						},
+						{
+							name: 'Starter',
+							includes: [{ offers: 'Own domain to connect' }, { offers: 'Own domain to connect' }, { offers: 'Visitor statistics' }],
+							curensy: '$',
+							rate: '29,99',
+							time: 'mo',
+						},
+					]
+				},
+			},
+		},
 	}
 </script>
 
@@ -72,20 +94,41 @@
 		display: flex;
 		flex-direction: row;
 		align-items: center;
+		justify-content: center;
+		flex-wrap: wrap;
 	}
+
 	article {
 		color: $white;
-		width: 33.3333%;
+		width: 30%;
 		border: 0.125rem solid $white;
+		margin: 0.3rem;
+		.info {
+			width: 72%;
+			height: 50vh;
+			margin: 0 auto;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: flex-start;
+			h2 {
+				margin: 1.75rem 0 3.125rem 0;
+				font-size: 2rem;
+				line-height: 130%;
+			}
+		}
 		.price {
 			width: 100%;
-			height: 14vh;
+			height: 10vh;
 			border: 0.125rem solid $white;
+			border-left: 0;
+			border-right: 0;
 			display: flex;
 			flex-direction: row;
 			justify-content: center;
 			align-items: center;
 			color: $white;
+
 			.curensy {
 				margin: 0 0.8rem 0 0;
 			}
@@ -106,7 +149,7 @@
 		}
 		button {
 			width: 100%;
-			height: 14vh;
+			height: 10vh;
 			border: 0.125rem solid $bg;
 			background-color: $white;
 			p {
@@ -130,20 +173,6 @@
 		h3 {
 			margin: 0 0 0 0.625rem;
 			font-size: 1.25rem;
-			line-height: 130%;
-		}
-	}
-	.info {
-		width: 72%;
-		height: 56vh;
-		margin: 0 auto;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: flex-start;
-		h2 {
-			margin: 1.75rem 0 3.125rem 0;
-			font-size: 2rem;
 			line-height: 130%;
 		}
 	}
