020aea9acdbffb8e24cbf07960f08614dd547da7
micromechanika <micromechanika@gmail.com>
2020-05-13 10:35:46 +0300

    mtg finish

--- components/index/maketorether.vue
+++ components/index/maketorether.vue
@@ -1,6 +1,14 @@
 <template>
 	<div class="maketogether">
-		<aside />
+		<aside>
+			<section>
+				<h2>Let’s make your website together!</h2>
+				<button>
+					<aside />
+					<p>Get Started</p>
+				</button>
+			</section>
+		</aside>
 	</div>
 </template>
 
@@ -22,5 +30,52 @@
 		background-repeat: no-repeat;
 		background-size: contain;
 		background-image: url('../../assets/img/maketogether.svg');
+	}
+	section {
+		top: 8.75rem;
+		left: 6rem;
+		position: relative;
+		z-index: 110;
+	}
+	h2 {
+		width: 50%;
+		margin: 0 0 0 0;
+		font-size: 4rem;
+		color: $bg;
+		position: relative;
+		top: 2.5rem;
+	}
+	button {
+		margin: 0 0 0 0;
+		position: relative;
+		top: 15rem;
+		width: 18rem;
+		height: 6rem;
+		background-color: transparent;
+		border: 0.1rem solid $bg;
+		padding: 0;
+
+		aside {
+			position: absolute;
+			top: 1rem;
+			left: 16rem;
+			width: 1rem;
+			height: 1rem;
+			background-position: center;
+			background-repeat: no-repeat;
+			background-size: contain;
+			background-image: url('../../assets/img/blackBow.svg');
+		}
+
+		p {
+			box-sizing: border-box;
+			display: block;
+			position: absolute;
+			left: 1rem;
+			bottom: 1rem;
+			font-size: 2rem;
+			margin: 0;
+			color: $bg;
+		}
 	}
 </style>
