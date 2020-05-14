31646db0feb3f338f051a8655b3d61d2a572500c
micromechanika <micromechanika@gmail.com>
2020-05-15 01:25:14 +0300

    add toggle button

--- components/prices/price.vue
+++ components/prices/price.vue
@@ -1,7 +1,15 @@
 <template>
 	<div class="price">
 		<h2>Our <span>prices</span> and packages </h2>
-		<aside></aside>
+
+		<section>
+			<h3 :class="{ active: togle }">Monthly</h3>
+			<aside :class="togle ? 'monthly' : 'yearly'">
+				<button @click="togle = !togle" />
+			</aside>
+			<h3 :class="{ active: !togle }">Yearly</h3>
+		</section>
+
 		<Packajes />
 	</div>
 </template>
@@ -11,6 +19,12 @@
 	export default {
 		name: 'Price',
 		components: { Packajes },
+		data() {
+			return {
+				togle: false,
+			}
+		},
+		methods: {},
 	}
 </script>
 
@@ -31,5 +45,52 @@
 		span {
 			color: $green;
 		}
+	}
+	section {
+		width: 75%;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		color: $white;
+	}
+	h3 {
+		font-size: 1.5rem;
+		line-height: 130%;
+		&:first-of-type {
+			margin-right: 2rem;
+		}
+		&:last-of-type {
+			margin-left: 2rem;
+		}
+	}
+
+	aside {
+		margin: 3rem 0;
+		border: 0.1rem solid $green;
+		width: 100px;
+		height: 60px;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+
+		button {
+			background-color: $green;
+			margin: 0.25rem;
+			border: 0.1rem solid $green;
+			width: 48%;
+			height: 90%;
+		}
+	}
+
+	.active {
+		color: $green;
+	}
+	.monthly {
+		justify-content: flex-start;
+	}
+
+	.yearly {
+		justify-content: flex-end;
 	}
 </style>
